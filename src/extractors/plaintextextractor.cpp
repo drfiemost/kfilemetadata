a58c814400aae5723713c018dbe5a9af73ca2afd
Leandro Nini <drfiemost@users.noreply.github.com>
2024-12-29 20:31:53 +0100

    Fix warning

--- src/extractors/plaintextextractor.cpp
+++ src/extractors/plaintextextractor.cpp
@@ -41,7 +41,7 @@ void PlainTextExtractor::extract(ExtractionResult* result)
     std::string line;
     int lines = 0;
 
-    std::ifstream fstream(QFile::encodeName(result->inputUrl()));
+    std::ifstream fstream(QFile::encodeName(result->inputUrl()).constData());
     if (!fstream.is_open()) {
         return;
     }
